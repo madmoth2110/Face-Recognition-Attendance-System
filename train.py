061669f535bc21511342dff785ddf75adbcd59f0
madmoth2110 <nishakharayat7@gmail.com>
2026-09-17 22:54:09 +0530

    Enhance training script to save names as JSON
    Added functionality to save names to a JSON file after training.

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import numpy as np
+import json
 
 path = "employees"
 
@@ -19,7 +20,7 @@ for file in os.listdir(path):
 
     if file.endswith(".jpg"):
 
-        name = file.split(".")[0]
+        name = os.path.splitext(file)[0]
 
         names[current_id] = name
 
@@ -28,9 +29,7 @@ for file in os.listdir(path):
             cv2.IMREAD_GRAYSCALE
         )
 
-        detected_faces = face_detector.detectMultiScale(
-            image
-        )
+        detected_faces = face_detector.detectMultiScale(image)
 
         for (x, y, w, h) in detected_faces:
 
@@ -42,6 +41,10 @@ for file in os.listdir(path):
 
         current_id += 1
 
+if len(faces) == 0:
+    print("No face images found.")
+    exit()
+
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 recognizer.train(
@@ -50,6 +53,9 @@ recognizer.train(
 )
 
 recognizer.save("trainer.yml")
+
+with open("names.json", "w") as file:
+    json.dump(names, file)
 
 print("Training completed")
 print(names)
